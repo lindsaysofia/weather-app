9b5a9bf39d8bb8f687bb6f620e1ba72309db3572
Lindsay Lopez <lopez95@berkeley.edu>
2021-07-18 15:43:48 -0700

    retrieved current weather data, now need to retrieve daily data

--- dist/main.js
+++ dist/main.js
@@ -15,7 +15,7 @@
   \**********************/
 /***/ (() => {
 
-eval("const OPEN_WEATHER_KEY = config.OPEN_WEATHER_KEY;\n\n//# sourceURL=webpack://weather-app/./src/index.js?");
+eval("const OPEN_WEATHER_KEY = config.OPEN_WEATHER_KEY;\nlet data;\n\nasync function getWeatherData(location) {\n  try {\n    const currentWeatherResponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${OPEN_WEATHER_KEY}`, {mode: 'cors'});\n    const currentWeatherData = await currentWeatherResponse.json();\n\n    const oneCallResponse = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${currentWeatherData.coord.lat}&lon=${currentWeatherData.coord.lon}&exclude=minutely,hourly,alerts&units=imperial&appid=${OPEN_WEATHER_KEY}`, {mode: 'cors'});\n    const oneCallData = await oneCallResponse.json();\n\n    data = {\n      current: {\n        temp : oneCallData.current.temp,\n        feels_like: oneCallData.current.feels_like,\n        description: oneCallData.current.weather[0].description,\n      }\n    }\n\n    console.log(data);\n  } catch (err) {\n    console.log(err);\n  }\n}\n\ngetWeatherData('San Diego');\n\n//# sourceURL=webpack://weather-app/./src/index.js?");
 
 /***/ })
 
--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 const OPEN_WEATHER_KEY = config.OPEN_WEATHER_KEY;
+let data;
+
+async function getWeatherData(location) {
+  try {
+    const currentWeatherResponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${OPEN_WEATHER_KEY}`, {mode: 'cors'});
+    const currentWeatherData = await currentWeatherResponse.json();
+
+    const oneCallResponse = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${currentWeatherData.coord.lat}&lon=${currentWeatherData.coord.lon}&exclude=minutely,hourly,alerts&units=imperial&appid=${OPEN_WEATHER_KEY}`, {mode: 'cors'});
+    const oneCallData = await oneCallResponse.json();
+
+    data = {
+      current: {
+        temp : oneCallData.current.temp,
+        feels_like: oneCallData.current.feels_like,
+        description: oneCallData.current.weather[0].description,
+      }
+    }
+
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+getWeatherData('San Diego');
