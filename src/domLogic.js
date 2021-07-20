c26eb304e34fcf357342dfe35f0de992f1b41005
Lindsay Lopez <lopez95@berkeley.edu>
2021-07-19 17:48:39 -0700

    current weather function working as expected

--- src/domLogic.js
+++ src/domLogic.js
@@ -22,9 +22,25 @@ const domLogic = (() => {
     date.classList.add('date');
     date.textContent = `${currentWeather ? 'Today, ' : ''}${formatDate(add(new Date(), {days: data.day}))}`;
 
-    current.appendChild(container);
+    icon.classList.add('icon');
+    icon.src = `http://openweathermap.org/img/wn/${data.icon}@2x.png`;
+
+    topTemp.classList.add('top', 'temp');
+    topTemp.innerHTML = `<span>${currentWeather ? data.temp : data.max_temp}</span>`;
+
+    bottomTemp.classList.add('bottom', 'temp');
+    bottomTemp.innerHTML = currentWeather ? `feels like <span>${data.feels_like}</span>` : `<span>${data.min_temp}</span>`;
+
+    description.classList.add('description');
+    description.textContent = data.description;
+
     container.appendChild(date);
-    
+    container.appendChild(icon);
+    container.appendChild(topTemp);
+    container.appendChild(bottomTemp);
+    container.appendChild(description);
+
+    current.appendChild(container);
   };
 
   const createInteractiveElements = () => {};
